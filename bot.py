d878b5a99dbd2b2d4f6e4daa51231dc9af4fed10
Sriram-PR <sriram.periannan@gmail.com>
2025-10-19 13:41:37 +0530

    testings

--- bot.py
+++ bot.py
@@ -38,8 +38,7 @@ intents.guilds = True
 
 # Shiny detection pattern - more robust than hardcoded Unicode
 # SHINY_PATTERN = re.compile(r"Vs\.[\s\u200B]*★")
-# SHINY_PATTERN = re.compile(r"Vs\.[\s\u200B]*\u2605", re.UNICODE)
-SHINY_PATTERN = re.compile(r"Vs\.[\s\u200B]*Nymble", re.UNICODE)
+SHINY_PATTERN = re.compile(r"Vs\.[\s\u200B]*\u2605", re.UNICODE)
 
 
 class GuildShinyConfig:
